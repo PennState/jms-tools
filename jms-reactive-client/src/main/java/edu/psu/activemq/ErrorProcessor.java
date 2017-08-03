ce407da1073bad9fae5aba45470b30b48f2b99d4
Shawn Eion Smith <ses44@psu.edu>
2017-08-03 10:09:52 -0400

    Adding tools for error handling

--- jms-reactive-client/src/main/java/edu/psu/activemq/ErrorProcessor.java
+++
@@ -1,9 +0,0 @@
-package edu.psu.activemq;
-
-import javax.jms.Message;
-
-interface ErrorProcessor {
-
-  void handleError(Message message);
-  void handleError(Message message, Throwable exception);
-}
--- jms-reactive-client/src/main/java/edu/psu/activemq/MessageProcessor.java
+++ jms-reactive-client/src/main/java/edu/psu/activemq/MessageProcessor.java
@@ -1,63 +1,104 @@
 package edu.psu.activemq;
 
 import javax.jms.Connection;
+import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
+import edu.psu.activemq.exception.UnableToProcessMessageException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public abstract class MessageProcessor {
 
   boolean process = true;
-  ErrorProcessor errorProcessor;
+  boolean handleErrors = false;
 
-  protected abstract void handleMessage(Message message);
+  protected abstract void handleMessage(Message message) throws UnableToProcessMessageException;
 
   public MessageProcessor(String ip, String transportName) {
     log.info("In the message producer constructor with ip = " + ip + " and transport name = " + transportName);
-    initialize(ip, transportName);
+    initialize(ip, transportName, null, null, null);
   }
 
-  public MessageProcessor(ErrorProcessor errorProcessor, String ip, String transportName) {
+  public MessageProcessor(String ip, String transportName, String errorIp, String errorTransportName, TransportType errorTransportType) {
     log.info("In the message producer constructor with ip = " + ip + " and transport name = " + transportName);
-    initialize(ip, transportName);
+    handleErrors = true;
+    initialize(ip, transportName, errorIp, errorTransportName, errorTransportType);
   }
-  
+
   public void terminate() {
     process = false;
   }
 
-  private void initialize(String ip, String transportName) {
+  private void initialize(String ip, String transportName, String errorIp, String errorTransportName, TransportType errorTransportType) {
 
     Thread t = new Thread(new Runnable() {
 
       public void run() {
+        
+        MessageConsumer consumer = null;
+        Connection connection = null;
+        
         try {
-          // log.info("Creating a connection at tcp://" + ip);
-          Connection connection = new ActiveMQConnectionFactory("tcp://" + ip).createConnection();
-          // log.info("Starting the connection");
+          connection = new ActiveMQConnectionFactory("tcp://" + ip).createConnection();
           connection.start();
-          // log.info("Creating a session");
           Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-          // log.info("Creating a queue: " + transportName);
 
           Queue destination = session.createQueue(transportName);
-
-          MessageConsumer consumer = session.createConsumer(destination);
-          //log.info("Starting the message processing loop");
-          while (process) {
-            // log.info("Calling consumer.receive");
-            Message message = consumer.receive();
-            // log.info("Calling handleMessage with " + message);
-            handleMessage(message);
-          }
+          consumer = session.createConsumer(destination);
         } catch (JMSException e) {
+          // TODO - revisit this
+          throw new RuntimeException("Failed to initialize processing queue");
+        }
+        
+        Connection errorConnection = null;
+        MessageProducer errorProducer = null;
+        if (handleErrors) {
+          try {
+            errorConnection = new ActiveMQConnectionFactory("tcp://" + ip).createConnection();
+            errorConnection.start();
+            Session errorSession = connection.createSession(false,  Session.AUTO_ACKNOWLEDGE);
+            
+            Destination errorDestination = null;
+            if (TransportType.TOPIC.equals(errorTransportType)) {
+              errorDestination = errorSession.createTopic(transportName);
+            } else {
+              errorDestination = errorSession.createQueue(transportName);
+            }
+            
+            
+            errorProducer = errorSession.createProducer(errorDestination);
+          } catch (JMSException e) {
+            // TODO - Revisit this
+            throw new RuntimeException("Failed to initialize the error endpoint");
+          }
+        }
+        
+        try {
+
+          Message message = null;
+          while (process) {
+            message = consumer.receive();
+            try {
+              handleMessage(message);
+            } catch (UnableToProcessMessageException e) {
+              if (errorProducer != null) {
+                //TODO - How do we format the message?
+              }
+            }
+          }
+
+          connection.close();
+          consumer.close();
+        } catch (JMSException e) {
+
           // TODO Auto-generated catch block
           e.printStackTrace();
         }
