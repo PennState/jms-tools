4c5fa10750cba305eafcb0b66c315c0a60da951b
Chris Pike <clp207@psu.edu>
2018-04-13 11:43:08 -0400

    add second try catch so exception in processor doesn't kill it and fail to send to error queue

--- jms-reactive-client/src/main/java/edu/psu/activemq/MessageProcessor.java
+++ jms-reactive-client/src/main/java/edu/psu/activemq/MessageProcessor.java
@@ -29,25 +29,25 @@ import lombok.extern.slf4j.Slf4j;
 @Data
 public abstract class MessageProcessor {
 
-  @Getter(value=AccessLevel.NONE)
-  @Setter(value=AccessLevel.NONE)
-  boolean process = true;  
-  
-  @Getter(value=AccessLevel.NONE)
-  @Setter(value=AccessLevel.NONE)
+  @Getter(value = AccessLevel.NONE)
+  @Setter(value = AccessLevel.NONE)
+  boolean process = true;
+
+  @Getter(value = AccessLevel.NONE)
+  @Setter(value = AccessLevel.NONE)
   boolean stopped = false;
-  
+
   String brokerUrl = null;
   String transportName = null;
   String errorTransportName = null;
-  TransportType errorTransportType = null;  
+  TransportType errorTransportType = null;
   String username;
   String password;
 
   protected abstract void handleMessage(Message message) throws UnableToProcessMessageException;
 
   public MessageProcessor() {
-    
+
   }
 
   public void terminate() {
@@ -56,118 +56,121 @@ public abstract class MessageProcessor {
 
   void initialize() {
     log.info("Initializing message processor...");
-    
+
     Thread t = new Thread(new Runnable() {
 
-      public void run() {        
+      public void run() {
         ActiveMQMessageConsumer consumer = null;
         ActiveMQConnection connection = null;
-        
+
         try {
           connection = (ActiveMQConnection) MessageHandler.buildActivemqConnection(brokerUrl, username, password);
-          
-          RedeliveryPolicy rd = new RedeliveryPolicy();          
+
+          RedeliveryPolicy rd = new RedeliveryPolicy();
           rd.setMaximumRedeliveries(2);
-          connection.getRedeliveryPolicyMap().put(new ActiveMQQueue(transportName), rd);
-          
+          connection.getRedeliveryPolicyMap()
+                    .put(new ActiveMQQueue(transportName), rd);
+
           connection.start();
           Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
 
           Queue destination = session.createQueue(transportName);
           consumer = (ActiveMQMessageConsumer) session.createConsumer(destination);
-          
+
         } catch (JMSException e) {
           log.info("Error creating message consumer", e);
           stopped = true;
           throw new RuntimeException("Failed to initialize processing queue");
         }
-        
+
         Connection errorConnection = null;
         MessageProducer errorProducer = null;
         if (errorTransportName != null) {
           try {
             errorConnection = MessageHandler.buildActivemqConnection(brokerUrl, username, password);
             errorConnection.start();
-            Session errorSession = connection.createSession(false,  Session.AUTO_ACKNOWLEDGE);
-            
+            Session errorSession = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
             Destination errorDestination = null;
             if (TransportType.TOPIC.equals(errorTransportType)) {
               errorDestination = errorSession.createTopic(errorTransportName);
             } else {
               errorDestination = errorSession.createQueue(errorTransportName);
             }
-                        
+
             errorProducer = errorSession.createProducer(errorDestination);
-          } catch (JMSException e) {            
+          } catch (JMSException e) {
             log.error("Error creating error producer", e);
             stopped = true;
             throw new RuntimeException("Failed to initialize the error endpoint");
           }
         }
-        
+
         try {
           Message message = null;
           while (process) {
             message = consumer.receive();
             try {
-              handleMessage(message);
-              consumer.acknowledge();
-            } catch (UnableToProcessMessageException upme) {
-              if (UnableToProcessMessageException.HandleAction.RETRY.equals(upme.getHandleAction())) {
-                message.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_DELAY, upme.getRetryWait());
-              } else if (UnableToProcessMessageException.HandleAction.DROP.equals(upme.getHandleAction())) {
-                continue;
-              } else {
-                //Go to the default action
-                throw upme;
+              try {
+                handleMessage(message);
+                consumer.acknowledge();
+              } catch (UnableToProcessMessageException upme) {
+                if (UnableToProcessMessageException.HandleAction.RETRY.equals(upme.getHandleAction())) {
+                  message.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_DELAY, upme.getRetryWait());
+                } else if (UnableToProcessMessageException.HandleAction.DROP.equals(upme.getHandleAction())) {
+                  continue;
+                } else {
+                  // Go to the default action
+                  throw upme;
+                }
               }
             } catch (Exception e) {
-              log.warn("Error processing message", e);                                          
+              log.warn("Error processing message", e);
               if (errorProducer != null) {
-                log.info("Sending to error queue");                
-                ActiveMQMessage msg = (ActiveMQMessage)message;     
+                log.info("Sending to error queue");
+                ActiveMQMessage msg = (ActiveMQMessage) message;
                 msg.setReadOnlyProperties(false);
                 msg.setStringProperty("error", e.getMessage());
                 msg.setStringProperty("errorStackTrace", getStackTrace(e));
                 errorProducer.send(msg);
                 consumer.acknowledge();
-              }
-              else{
+              } else {
                 consumer.rollback();
               }
             }
           }
 
         } catch (Exception e) {
-          stopped = true;          
+          stopped = true;
           log.error("Processor exception processing message.", e);
-          
-          try{
-            consumer.rollback();          
+
+          try {
+            consumer.rollback();
+          } catch (JMSException e1) {
           }
-          catch(JMSException e1) {}          
-        }
-        finally{
+        } finally {
           try {
             consumer.close();
-          } catch (JMSException e) {}  
+          } catch (JMSException e) {
+          }
           try {
             connection.close();
-          } catch (JMSException e) {}        
+          } catch (JMSException e) {
+          }
         }
       }
     });
     t.start();
   }
-  
-  private String getStackTrace(Exception e){
+
+  private String getStackTrace(Exception e) {
     StringWriter sw = new StringWriter();
     PrintWriter pw = new PrintWriter(sw);
     e.printStackTrace(pw);
     return sw.toString();
   }
-  
-  public boolean isStopped(){
+
+  public boolean isStopped() {
     return stopped;
   }
 }
