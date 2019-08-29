99af1829c7a9b8f0fdd526d353c77d58677ff8d1
Craig Benner <cpb113@psu.edu>
2019-08-29 14:07:21 -0400

    setting default retries to 12

--- jms-reactive-client/src/main/java/edu/psu/activemq/exception/UnableToProcessMessageException.java
+++ jms-reactive-client/src/main/java/edu/psu/activemq/exception/UnableToProcessMessageException.java
@@ -35,7 +35,7 @@ public class UnableToProcessMessageException extends RuntimeException {
 
   @Getter
   @Setter
-  Integer numberOfRetries = null;
+  Integer numberOfRetries = 12;
 
   @Getter
   @Setter
