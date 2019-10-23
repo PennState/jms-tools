b0c82871b10cc627e9649945f3afdda239ddb15f
Michael Zalewski <mez5001@psu.edu>
2019-10-23 09:01:18 -0400

    Removing unused import causing compilation errors

--- jms-reactive-client/src/main/java/edu/psu/activemq/util/PropertyUtil.java
+++ jms-reactive-client/src/main/java/edu/psu/activemq/util/PropertyUtil.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.assertj.core.util.Arrays;
-
 /*
  * Copyright (c) 2018 by The Pennsylvania State University
  * 
